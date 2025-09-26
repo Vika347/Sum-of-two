bc68cb73a8fe609166909076e01ee78c18c3dd31
Vika347 <vika.rodina08@mail.ru>
2025-09-26 13:47:42 +0300

    Delete Lab1.py

--- Lab1.py
+++
@@ -1,9 +0,0 @@
-def sumdva(numbs, target):
-    if (type(target) == int) and (type(numbs) == list) and (len(numbs) > 1):
-        if all(type(numbs[i]) == int for i in range(0, len(numbs))):
-            for i in range(0, len(numbs)):
-                for j in range(i + 1, len(numbs)):
-                    if numbs[i] + numbs[j] == target:
-                        return [i, j]
-    return None
-
